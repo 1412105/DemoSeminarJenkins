393e356ff692c906605c1d61193946d61bbb002f
Unknown <1412105@student.hcmus.edu.vn>
2017-10-19 22:51:58 +0700

    Add demo file01

---
+++ jk/HelloWorld.java
@@ -0,0 +1,14 @@
+package jk;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("Welcome to Jenkins");
+		// TestClass t = new TestClass();
+		// System.out.println(t.value.charAt(0));
+	}
+}
+//
+// class TestClass {
+// 	String value;
+// }

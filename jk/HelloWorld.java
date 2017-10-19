f320778a33df863fba3c00bef95036c45c664818
Unknown <1412105@student.hcmus.edu.vn>
2017-10-20 02:43:18 +0700

    Test #2

--- jk/HelloWorld.java
+++ jk/HelloWorld.java
@@ -3,5 +3,11 @@ package jk;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld, Welcome to Jenkins, TEST POLL SCM");
+		TestClass t = new TestClass();
+		System.out.println(t.value.charAt(0));
 	}
+}
+
+class TestClass {
+	String value;
 }

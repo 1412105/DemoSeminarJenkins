b73fb8538064a477ee7b1e95d6d2050afc94a8b7
Unknown <1412105@student.hcmus.edu.vn>
2017-10-19 23:47:42 +0700

    Add demo file02

--- jk/HelloWorld.java
+++ jk/HelloWorld.java
@@ -2,13 +2,6 @@ package jk;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		System.out.println("Welcome to Jenkins");
-		// TestClass t = new TestClass();
-		// System.out.println(t.value.charAt(0));
+		System.out.println("HelloWorld, Welcome to Jenkins");
 	}
 }
-//
-// class TestClass {
-// 	String value;
-// }
